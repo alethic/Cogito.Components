edf0f0be5f770b847eec571c49d58b757041d137
Jerome Haltom <jhaltom@alethic.solutions>
2024-12-20 22:29:59 -0600

    Add a sample app.

---
+++ src/Cogito.Components.Sample1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+using Cogito.Autofac;
+using Cogito.Autofac.DependencyInjection;
+
+using Microsoft.Extensions.Hosting;
+
+namespace Cogito.Components.Sample1
+{
+
+    public static class Program
+    {
+        public static async Task Main(string[] args) => await Host.CreateDefaultBuilder(args)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory(b => b.RegisterAllAssemblyModules()))
+            .RunConsoleAsync();
+
+    }
+
+}
